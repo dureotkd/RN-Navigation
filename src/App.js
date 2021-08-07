2a3d67cdfb7e120b503da30d5ee6a313f24753f5
duerotkd <dureotkd123@naver.com>
2021-08-08 00:22:45 +0900

    기기연결 성공

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  console.log("HI");
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: "blue" }}>Hi, My Name is Sungmin</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
